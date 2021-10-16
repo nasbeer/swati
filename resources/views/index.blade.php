a614839ba9dc5e82e981b765c50a03f19da51d1f
admin <nasbeer@jubna.com>
2021-10-16 17:25:53 +0400

    updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,43 +1,4 @@
-<?php
 
-$auth_token = 'pubkey-e8N8Z6eGMW1Rc4G9H3Y1MmF7iph9H0';
-
-$headers   = array();
-$headers[] = 'Authorization: Token ' . $auth_token;
-$curl      = curl_init();
-curl_setopt_array($curl, [
-        CURLOPT_URL => "http://swati.centraqa.com/api/checkout/products/",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_SSL_VERIFYPEER =>FALSE,
-        CURLOPT_SSL_VERIFYHOST => FALSE,
-       // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-	// CURLOPT_POSTFIELDS => ["price_amount=1229.0",
-	// 						"price_currency=EUR",
-	// 						"title=Order #390551",
-	// 						"receive_currency=EUR",
-	// 						"callback_url=https://coingate.requestcatcher.com/payment",
-	// 						"success_url=http://example.com/success" ,
-	// 						"cancel_url=http://example.com/cancel",
-	// 						"order_id=390551",
-	// 						"description=1 x Apple iPhone 12 "
-	// ],
-	CURLOPT_HTTPHEADER => [
-		"Access-Control-Allow-Origin : http://localhost:1229 ",
-	//	'Authorization: Token ' . $auth_token,
-		 "X-Centra-Request-ID: 1_375b289374dfd7183002e508e30a775b",
-		 "X-Correlation-ID : centra_1_375b289374dfd7183002e508e30a775b",
-		// "x-rapidapi-key: 4787fb1d34msh5d865994d592badp1f6f93jsnbcbc449e8362"
-	],
-]);
-$json = curl_exec($curl);
-$response = json_decode($json, true);
-$err = curl_error($curl);
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -1251,10 +1212,7 @@ $err = curl_error($curl);
         <section class="swati-section alternate text-center alternate" id="productCarouselSection">
             <div class="container ">
                 <h2 class="primary__title text-center mb-3">You may also like</h2>
-
-                <?php 
-                    echo "<pre> Response"; print_r($response); echo "</pre>";
-                ?>
+                <div id="getbtchashval" class="getbtchashval text-center"></div>
                 <span class="clerk" data-template="@homepage-slider"></span>
             </div>
         </section>
